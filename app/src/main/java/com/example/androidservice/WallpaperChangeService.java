2ddf89ad75907cf10ae7b672c78f9dda7af0266f
Vyan Insya Nur M <vyaninsya.nurmuhammad@gmail.com>
2020-04-29 11:37:41 +0700

    start

---
+++ app/src/main/java/com/example/androidservice/WallpaperChangeService.java
@@ -0,0 +1,73 @@
+package com.example.androidservice;
+
+import android.app.Service;
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.IBinder;
+
+
+public class WallpaperChangeService extends Service implements Runnable {
+    /*referensi gambar walpaper disimpan dalam sebuah array, wallpaper1 dan wallpaper2
+   adalah nama file png atau jpeg anda */
+    private int wallpaperId[] =  {R.drawable.wallpapersatu, R.drawable.wallpaperdua} ;
+    /*Deklarasi variabel yang digunakan untuk menyimpan durasi yang dipilih user */
+    private int waktu;
+    /*Deklarasi variabel flag untuk check gambar mana yang akan ditampilkan berikutnya */
+    private int FLAG=0;
+    private Thread t;
+    /*Deklarasi variabel flag untuk check gambar mana yang akan ditampilkan berikutnya */
+    private Bitmap gambar;
+    private Bitmap gambar1;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+        /**/
+        Bundle bundle = intent.getExtras();
+        /**/
+        waktu = bundle.getInt("durasi");
+        gambar = BitmapFactory.decodeResource(getResources(), wallpaperId[0]);
+        gambar1 = BitmapFactory.decodeResource(getResources(), wallpaperId[1]);
+        /**/
+
+        t = new Thread(WallpaperChangeService.this);
+        t.start();
+        return START_STICKY_COMPATIBILITY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        System.exit(0);
+    }
+    /**/
+
+    @Override
+    public void run() {
+        WallpaperManager myWallpaper;
+        myWallpaper = WallpaperManager.getInstance(this);
+        try {
+            while(true) {
+                if(FLAG==0) {
+                    myWallpaper.setBitmap(gambar);
+                    FLAG++;
+                }
+                else {
+                    myWallpaper.setBitmap(gambar1);
+                    FLAG--;
+                }
+                Thread.sleep(100*waktu);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
